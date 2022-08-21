b7557b9018587d3b6d351f06e9b9bbdde9a6755a
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-21 19:43:35 -0300

    feat: create new component text

---
+++ packages/react/Text/src/text.types.ts
@@ -0,0 +1,18 @@
+import { ReactNode, HTMLAttributes } from "react";
+
+export interface TextProps extends HTMLAttributes<HTMLParagraphElement> {
+  /** Element to be rendered inside the Text component */
+  children: ReactNode;
+  /** Type of html tag to create for the Text component */
+  as?: "p" | "span";
+  /** Text alignment in content */
+  textAlign?: "left" | "right" | "center" | "justify";
+  /** Bold font for the text component */
+  bold?: boolean;
+  /** Text size */
+  size?: "small" | "caption" | "base";
+  /** Text color */
+  appearance?: "primary" | "success" | "warning" | "danger" | "neutral";
+  /** Text line height */
+  lineHeight?: "small" | "caption" | "base";
+}
