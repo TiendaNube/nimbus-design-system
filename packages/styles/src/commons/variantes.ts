ac2cbc89f67dde08e6d3066ed7c1d85c05fef8a6
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-21 19:26:26 -0300

    chore: updating base theme for components

---
+++ packages/styles/src/commons/variantes.ts
@@ -0,0 +1,6 @@
+export const textAlign = {
+  left: { textAlign: "left" },
+  right: { textAlign: "right" },
+  center: { textAlign: "center" },
+  justify: { textAlign: "justify" },
+} as any;
--- packages/styles/src/themes/base.css.ts
+++ packages/styles/src/themes/base.css.ts
@@ -19,11 +19,56 @@ export const varsThemeBase = createGlobalTheme(":root", {
       low: colors.primary.low.value as string,
       high: colors.primary.high.value as string,
     },
+    success: {
+      bg: colors.success.bg.value as string,
+      surface: colors.success.surface.value as string,
+      surfaceHighlight: colors.success["surface-highlight"].value as string,
+      interactive: colors.success.interactive.value as string,
+      interactiveHover: colors.success["interactive-hover"].value as string,
+      interactivePressed: colors.success["interactive-pressed"].value as string,
+      low: colors.success.low.value as string,
+      high: colors.success.high.value as string,
+    },
+    warning: {
+      bg: colors.warning.bg.value as string,
+      surface: colors.warning.surface.value as string,
+      surfaceHighlight: colors.warning["surface-highlight"].value as string,
+      interactive: colors.warning.interactive.value as string,
+      interactiveHover: colors.warning["interactive-hover"].value as string,
+      interactivePressed: colors.warning["interactive-pressed"].value as string,
+      low: colors.warning.low.value as string,
+      high: colors.warning.high.value as string,
+    },
+    danger: {
+      bg: colors.danger.bg.value as string,
+      surface: colors.danger.surface.value as string,
+      surfaceHighlight: colors.danger["surface-highlight"].value as string,
+      interactive: colors.danger.interactive.value as string,
+      interactiveHover: colors.danger["interactive-hover"].value as string,
+      interactivePressed: colors.danger["interactive-pressed"].value as string,
+      low: colors.danger.low.value as string,
+      high: colors.danger.high.value as string,
+    },
+    neutral: {
+      bg: colors.neutral.bg.value as string,
+      surface: colors.neutral.surface.value as string,
+      disabled: colors.neutral.disabled.value as string,
+      interactive: colors.neutral.interactive.value as string,
+      interactiveHover: colors.neutral["interactive-hover"].value as string,
+      interactivePressed: colors.neutral["interactive-pressed"].value as string,
+      low: colors.neutral.low.value as string,
+      high: colors.neutral.high.value as string,
+    },
   },
   fontFamily: {
     centranube: fontFamily.value as string,
   },
   fontSize: {
+    body: {
+      small: fontSize.body.small.value as string,
+      caption: fontSize.body.caption.value as string,
+      base: fontSize.body.base.value as string,
+    },
     title: {
       h1: fontSize.title.h1.value as string,
       h2: fontSize.title.h2.value as string,
@@ -39,6 +84,11 @@ export const varsThemeBase = createGlobalTheme(":root", {
     bold: fontWeight.bold.value as string,
   },
   lineWeight: {
+    body: {
+      small: lineWeight.body.small.value as string,
+      caption: lineWeight.body.caption.value as string,
+      base: lineWeight.body.base.value as string,
+    },
     title: {
       h1: lineWeight.title.h1.value as string,
       h2: lineWeight.title.h2.value as string,
