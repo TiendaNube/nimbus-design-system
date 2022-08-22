ce3c0453ce91c7951f168a7152006e8ba75ea879
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-22 17:40:20 -0300

    feat: created new Text.Skeleton subcomponent

---
+++ packages/react/Title/src/fragments/TitleSkeleton/titleSkeleton.definitions.ts
@@ -0,0 +1,26 @@
+export const sizes = {
+  h1: {
+    width: "132px",
+    height: "44px",
+  },
+  h2: {
+    width: "99px",
+    height: "28px",
+  },
+  h3: {
+    width: "83px",
+    height: "28px",
+  },
+  h4: {
+    width: "74px",
+    height: "24px",
+  },
+  h5: {
+    width: "66px",
+    height: "20px",
+  },
+  h6: {
+    width: "58px",
+    height: "18px",
+  },
+};
