4673004e118dde1bc375661988f6e581672687d3
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-22 17:52:37 -0300

    docs: created new Skeleton subcomponent

---
+++ packages/react/Skeleton/src/skeleton.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+import { Skeleton } from "./Skeleton";
+
+export default {
+  title: "Atomic/Skeleton",
+  component: Skeleton,
+  parameters: {
+    withA11y: { decorators: [withA11y] },
+  },
+} as ComponentMeta<typeof Skeleton>;
+
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <Skeleton {...args} />
+);
+
+export const skeleton = Template.bind({});
+skeleton.args = {
+  height: "50px",
+  width: "150px",
+};
