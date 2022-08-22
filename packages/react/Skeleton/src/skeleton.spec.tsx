e8ba43cd7e4d885b251874bf1846eac75708cf2e
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-22 17:53:21 -0300

    test: created new Skeleton subcomponent

---
+++ packages/react/Skeleton/src/skeleton.spec.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { Skeleton } from "./Skeleton";
+import { SkeletonProps } from "./skeleton.types";
+
+const makeSut = (props: SkeletonProps) => {
+  render(<Skeleton {...props} data-testid="skeleton-element" />);
+};
+
+describe("GIVEN <Skeleton />", () => {
+  describe("WHEN rendered", () => {
+    it("THEN should ensure dynamic height and width properties are applied", () => {
+      makeSut({
+        height: "10rem",
+        width: "50rem",
+      });
+      const skeleton = screen.getByTestId("skeleton-element");
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--width__\w{0,9}: 50rem;/
+      );
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--height__\w{0,9}: 10rem;/
+      );
+    });
+
+    it("THEN should ensure dynamic border radius properties are applied", () => {
+      makeSut({
+        height: "10rem",
+        width: "50rem",
+        borderRadius: "4px",
+      });
+      const skeleton = screen.getByTestId("skeleton-element");
+      expect(skeleton.getAttribute("style")).toMatch(
+        /--borderRadius__\w{0,9}: 4px;/
+      );
+    });
+  });
+});
