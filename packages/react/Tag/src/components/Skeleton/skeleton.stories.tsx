9d25befababf67b352f83065bfb8561c4b0b2b17
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-24 19:53:06 -0300

    docs: create new component Tag.Skeleton

---
+++ packages/react/Tag/src/components/Skeleton/skeleton.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+import { Skeleton } from "./Skeleton";
+
+export default {
+  title: "Atomic/Tag/Tag.Skeleton",
+  component: Skeleton,
+  parameters: {
+    withA11y: { decorators: [withA11y] },
+  },
+} as ComponentMeta<typeof Skeleton>;
+
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <Skeleton {...args} />
+);
+
+export const base = Template.bind({});
+base.args = {};
