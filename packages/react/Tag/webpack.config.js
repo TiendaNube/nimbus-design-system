84db301262fd3aa9007756fa97b6221df9d7de42
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-24 19:54:25 -0300

    feat: create new component Tag

---
+++ packages/react/Tag/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    "./index": "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "nimbus-ds",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new VanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      "@nimbus-ds/skeleton": path.resolve(__dirname, "../skeleton/src"),
+      "@nimbus-ds/styles": path.resolve(__dirname, "../../styles/src"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserJSPlugin()],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+};
