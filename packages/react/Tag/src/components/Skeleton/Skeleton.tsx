12467688d5c41667b92cdd8aab254e1b26fedc50
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-24 19:51:36 -0300

    feat: create new component Tag.Skeleton

---
+++ packages/react/Tag/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Skeleton as SkeletonBase } from "@nimbus-ds/skeleton";
+import { SkeletonProps } from "./skeleton.types";
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  width,
+  height,
+  "data-testid": dataTestId,
+}) => (
+  <SkeletonBase
+    width={width ?? "3.313rem"}
+    height={height ?? "1rem"}
+    borderRadius="0.813rem"
+    data-testid={dataTestId}
+  />
+);
+
+export { Skeleton };
