2455c7f54edb5494ef6077830f5fcb735616797f
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-25 23:13:24 -0300

    feat: create new component Badge.Skeleton

---
+++ packages/react/Badge/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Skeleton as SkeletonBase } from "@nimbus-ds/skeleton";
+import { SkeletonProps } from "./skeleton.types";
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  width,
+  height,
+  "data-testid": dataTestId,
+}) => (
+  <SkeletonBase
+    width={width ?? "1.5rem"}
+    height={height ?? "1.25rem"}
+    borderRadius="0.813rem"
+    data-testid={dataTestId}
+  />
+);
+
+export { Skeleton };
