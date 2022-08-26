8c0185a292c71edce045ed591aac69346124ebae
Júnior Conquista <conquista.engcomp@gmail.com>
2022-08-26 08:39:26 -0300

    feat: added Centranube font in storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -6,4 +6,7 @@ export default create({
   brandTitle: "Design System Nimbus",
   brandUrl: "https://nimbus.nuvemshop.com.br",
   brandImage: LOGO,
+
+  fontBase:
+    "CentraNube, -apple-system, BlinkMacSystemFont, Roboto, 'Helvetica Neue', arial, sans-serif",
 });
