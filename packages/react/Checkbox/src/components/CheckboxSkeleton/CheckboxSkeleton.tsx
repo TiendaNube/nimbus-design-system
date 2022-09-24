c0e1b56990fb6a2951b31085ebed06bd16b50114
Júnior Conquista <conquista.engcomp@gmail.com>
2022-09-24 10:52:41 -0300

    feat: create new component Checkbox.Skeleton

---
+++ packages/react/Checkbox/src/components/CheckboxSkeleton/CheckboxSkeleton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Skeleton } from "@nimbus-ds/skeleton";
+import { checkbox } from "@nimbus-ds/styles";
+
+import { CheckboxSkeletonProps } from "./checkboxSkeleton.types";
+
+const CheckboxSkeleton: React.FC<CheckboxSkeletonProps> = ({
+  width,
+  "data-testid": dataTestId,
+}) => (
+  <div className={checkbox.style.container}>
+    <Skeleton width="1rem" height="1rem" borderRadius="0.25rem" />
+    <Skeleton
+      width={width ?? "2rem"}
+      height="1rem"
+      borderRadius="0.25rem"
+      data-testid={dataTestId}
+    />
+  </div>
+);
+
+export { CheckboxSkeleton };
