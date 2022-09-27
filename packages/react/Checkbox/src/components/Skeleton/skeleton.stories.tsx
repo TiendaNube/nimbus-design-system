6e434119a8f7208428e2ac3b37606e3257c49788
Júnior Conquista <conquista.engcomp@gmail.com>
2022-09-27 11:39:39 -0300

    chore: adjusted component native typing for InputHTMLAttributes

--- packages/react/Checkbox/src/components/Skeleton/skeleton.stories.tsx
+++ packages/react/Checkbox/src/components/Skeleton/skeleton.stories.tsx
@@ -5,7 +5,7 @@ import { withA11y } from "@storybook/addon-a11y";
 import { Skeleton } from "./Skeleton";
 
 export default {
-  title: "Skeleton/Label",
+  title: "Skeleton/Checkbox",
   component: Skeleton,
   parameters: {
     withA11y: { decorators: [withA11y] },
@@ -16,5 +16,5 @@ const Template: ComponentStory<typeof Skeleton> = (args) => (
   <Skeleton {...args} />
 );
 
-export const Label = Template.bind({});
-Label.args = {};
+export const checkbox = Template.bind({});
+checkbox.args = {};
