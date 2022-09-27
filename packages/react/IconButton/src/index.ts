d285808d94f695d84be4a64fd870b7f35938f0f9
Júnior Conquista <conquista.engcomp@gmail.com>
2022-09-27 10:26:22 -0300

    feat: create new component IconButton

---
+++ packages/react/IconButton/src/index.ts
@@ -0,0 +1,6 @@
+import { IconButton } from "./IconButton";
+import "@nimbus-ds/styles/packages/iconButton/index.css";
+
+export { IconButton } from "./IconButton";
+export type { IconButtonProps } from "./iconButton.types";
+export default IconButton;
