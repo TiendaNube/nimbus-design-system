ec4e8648bccc44bf0f62d7c3e1dcbfd5a3858576
Júnior Conquista <conquista.engcomp@gmail.com>
2022-09-28 16:51:19 -0300

    chore: removed background colors from primary, success, warning and danger scales from icon pack

--- packages/styles/src/packages/icon/icon.sprinkle.css.ts
+++ packages/styles/src/packages/icon/icon.sprinkle.css.ts
@@ -2,28 +2,24 @@ import { defineProperties, createSprinkles } from "@vanilla-extract/sprinkles";
 import { properties as colors } from "../../sprinkles/colors.css";
 
 export const properties = {
-  "primary.background": colors["primary.background"],
   "primary.interactive": colors["primary.interactive"],
   "primary.textLow": colors["primary.textLow"],
   "primary.textHigh": colors["primary.textHigh"],
 
-  "success.background": colors["success.background"],
-  "success.interactive": colors["primary.interactive"],
+  "success.interactive": colors["success.interactive"],
   "success.textLow": colors["success.textLow"],
   "success.textHigh": colors["success.textHigh"],
 
-  "warning.background": colors["warning.background"],
-  "warning.interactive": colors["primary.interactive"],
+  "warning.interactive": colors["warning.interactive"],
   "warning.textLow": colors["warning.textLow"],
   "warning.textHigh": colors["warning.textHigh"],
 
-  "danger.background": colors["danger.background"],
-  "danger.interactive": colors["primary.interactive"],
+  "danger.interactive": colors["danger.interactive"],
   "danger.textLow": colors["danger.textLow"],
   "danger.textHigh": colors["danger.textHigh"],
 
   "neutral.background": colors["neutral.background"],
-  "neutral.interactive": colors["primary.interactive"],
+  "neutral.interactive": colors["neutral.interactive"],
   "neutral.textLow": colors["neutral.textLow"],
   "neutral.textHigh": colors["neutral.textHigh"],
 };
