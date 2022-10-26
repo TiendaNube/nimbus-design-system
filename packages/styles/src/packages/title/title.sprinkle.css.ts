db5598b85ebc30a98b7ed3271f729306014b9b91
Juan Manuel Gallego <juanma.gallego@tiendanube.com>
2022-10-26 17:43:48 -0300

    feat: add new colors to title component api

--- packages/styles/src/packages/title/title.sprinkle.css.ts
+++ packages/styles/src/packages/title/title.sprinkle.css.ts
@@ -5,10 +5,15 @@ import { properties as textAlignProperties } from "../../sprinkles/textAlign.css
 
 const colorsProperties = {
   "primary.textHigh": colors["primary.textHigh"],
+  "primary.textLow": colors["primary.textLow"],
   "success.textHigh": colors["success.textHigh"],
+  "success.textLow": colors["success.textLow"],
   "warning.textHigh": colors["warning.textHigh"],
+  "warning.textLow": colors["warning.textLow"],
   "danger.textHigh": colors["danger.textHigh"],
+  "danger.textLow": colors["danger.textLow"],
   "neutral.textHigh": colors["neutral.textHigh"],
+  "neutral.textLow": colors["neutral.textLow"],
 };
 
 export const sprinkle = createSprinkles(
