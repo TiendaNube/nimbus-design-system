6ebdb8acce4c64c5bc70606d0e91c7e4b49b9c18
Júnior Conquista <conquista.engcomp@gmail.com>
2022-10-28 09:55:08 -0300

    feat: create new component FileUploader

---
+++ packages/react/FileUploader/src/fileUploader.types.ts
@@ -0,0 +1,23 @@
+import { InputHTMLAttributes } from "react";
+import { fileUploader } from "@nimbus-ds/styles";
+import { Skeleton } from "./components";
+
+export interface FileUploaderComponents {
+  Skeleton: typeof Skeleton;
+}
+
+export interface FileUploaderProps
+  extends InputHTMLAttributes<HTMLInputElement> {
+  /** Provide additional context to the action */
+  placeholder?: string;
+  /** Accept file types */
+  accept?: string;
+  /** Height value of the file uploader. Defaults to 100% */
+  height?: string;
+  /** Width value of the file uploader. Defaults to 100% */
+  width?: string;
+  /** Permitted aspect ratios for the size of the file uploader */
+  aspectRatio?: keyof typeof fileUploader.properties.aspectRatio;
+  /** Defines the position of the placeholder in relation to the icon */
+  flexDirection?: keyof typeof fileUploader.properties.flexDirection;
+}
