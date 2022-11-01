7e79c594d886cf17f769e7918eeea0fdd90e0992
Júnior Conquista <conquista.engcomp@gmail.com>
2022-11-01 14:01:09 -0300

    test: create new component Card

--- packages/react/Card/src/card.spec.tsx
+++ packages/react/Card/src/card.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { Card } from "./Card";
 import { CardProps } from "./card.types";
@@ -10,83 +10,126 @@ const makeSut = (rest: CardProps) => {
 
 describe("GIVEN <Card />", () => {
   describe("WHEN rendered", () => {
-    it("THEN should correctly render the submitted placeholder", () => {
-      makeSut({ children: <div>children</div> });
-      // expect(screen.getByText("my placeholder")).toBeDefined();
-      // expect(
-      //   screen.getByTestId<HTMLLabelElement>("file-uploader-container").htmlFor
-      // ).toEqual("input-file");
+    it("THEN should correctly render the submitted content", () => {
+      makeSut({ children: <div>My content</div> });
+      expect(screen.getByText("My content")).toBeDefined();
     });
 
-    // it("THEN should properly disable FileUploader", () => {
-    //   makeSut();
-    //   expect(
-    //     screen.getByTestId("file-uploader-container").getAttribute("class")
-    //   ).toContain("disabled");
-    //   expect(
-    //     screen.getByTestId<HTMLLabelElement>("file-uploader-container").htmlFor
-    //   ).toContain("disabled");
-    //   expect(
-    //     screen.getByTestId<HTMLInputElement>("file-uploader-element").disabled
-    //   ).toBeTruthy();
-    // });
+    describe("THEN should correctly render the submitted padding", () => {
+      it("THEN should correctly render the padding default", () => {
+        makeSut({ children: "My content" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("padding_base");
+      });
+
+      it("THEN should correctly render the padding none", () => {
+        makeSut({ padding: "none", children: "My content" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("padding_none");
+      });
+
+      it("THEN should correctly render the padding base", () => {
+        makeSut({ padding: "base", children: "My content" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("padding_base");
+      });
+    });
+
+    describe("THEN should correctly render the submitted backgroundColor", () => {
+      it("THEN should correctly render the backgroundColor default", () => {
+        makeSut({ children: "My content" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_neutral.background");
+      });
+
+      it("THEN should correctly render the backgroundColor primary.surface", () => {
+        makeSut({ children: "My content", backgroundColor: "primary.surface" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_primary.surface");
+      });
+
+      it("THEN should correctly render the backgroundColor primary.surfaceHighlight", () => {
+        makeSut({
+          children: "My content",
+          backgroundColor: "primary.surfaceHighlight",
+        });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_primary.surfaceHighlight");
+      });
+
+      it("THEN should correctly render the backgroundColor success.surface", () => {
+        makeSut({ children: "My content", backgroundColor: "success.surface" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_success.surface");
+      });
+
+      it("THEN should correctly render the backgroundColor success.surfaceHighlight", () => {
+        makeSut({
+          children: "My content",
+          backgroundColor: "success.surfaceHighlight",
+        });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_success.surfaceHighlight");
+      });
+
+      it("THEN should correctly render the backgroundColor warning.surface", () => {
+        makeSut({ children: "My content", backgroundColor: "warning.surface" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_warning.surface");
+      });
+
+      it("THEN should correctly render the backgroundColor warning.surfaceHighlight", () => {
+        makeSut({
+          children: "My content",
+          backgroundColor: "warning.surfaceHighlight",
+        });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_warning.surfaceHighlight");
+      });
+
+      it("THEN should correctly render the backgroundColor danger.surface", () => {
+        makeSut({ children: "My content", backgroundColor: "danger.surface" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_danger.surface");
+      });
+
+      it("THEN should correctly render the backgroundColor danger.surfaceHighlight", () => {
+        makeSut({
+          children: "My content",
+          backgroundColor: "danger.surfaceHighlight",
+        });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_danger.surfaceHighlight");
+      });
+
+      it("THEN should correctly render the backgroundColor neutral.surface", () => {
+        makeSut({ children: "My content", backgroundColor: "neutral.surface" });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_neutral.surface");
+      });
+
+      it("THEN should correctly render the backgroundColor neutral.surfaceHighlight", () => {
+        makeSut({
+          children: "My content",
+          backgroundColor: "neutral.surfaceHighlight",
+        });
+        expect(
+          screen.getByTestId("card-element").getAttribute("class")
+        ).toContain("backgroundColor_neutral.surfaceHighlight");
+      });
+    });
   });
-
-  // describe("THEN should correctly render the submitted aspectRatio", () => {
-  //   it("THEN should correctly render the aspectRatio default", () => {
-  //     makeSut();
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_1-1");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 1-1", () => {
-  //     makeSut({ aspectRatio: "1-1" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_1-1");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 1-2", () => {
-  //     makeSut({ aspectRatio: "1-2" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_1-2");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 16-9", () => {
-  //     makeSut({ aspectRatio: "16-9" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_16-9");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 2-1", () => {
-  //     makeSut({ aspectRatio: "2-1" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_2-1");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 3-4", () => {
-  //     makeSut({ aspectRatio: "3-4" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_3-4");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 4-3", () => {
-  //     makeSut({ aspectRatio: "4-3" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_4-3");
-  //   });
-
-  //   it("THEN should correctly render the aspectRatio 9-16", () => {
-  //     makeSut({ aspectRatio: "9-16" });
-  //     expect(
-  //       screen.getByTestId("file-uploader-container").getAttribute("class")
-  //     ).toContain("aspectRatio_9-16");
-  //   });
-  // });
 });
