6f6f61ad85e451ab23c2a7fdff60572b146e42ba
Júnior Conquista <conquista.engcomp@gmail.com>
2022-12-01 10:17:43 -0300

    refactor: changed component base typing to extends to native button types

--- packages/react/IconButton/src/iconButton.types.ts
+++ packages/react/IconButton/src/iconButton.types.ts
@@ -1,15 +1,18 @@
-import { ReactNode, AnchorHTMLAttributes } from "react";
+import { ReactNode, ButtonHTMLAttributes } from "react";
+import { IconButtonSprinkle } from "@nimbus-ds/styles";
 
 import { Skeleton } from "./components";
+
+type Extends = IconButtonSprinkle &
+  ButtonHTMLAttributes<HTMLButtonElement | HTMLDivElement>;
 
 export interface IconButtonComponents {
   Skeleton: typeof Skeleton;
 }
 
-export interface IconButtonProps
-  extends AnchorHTMLAttributes<HTMLAnchorElement | HTMLDivElement> {
+export interface IconButtonProps extends Extends {
   /** Type of html tag to create for the Icon Button component */
-  as?: "a" | "div";
+  as?: "button" | "div";
   /** Icon Button SVG */
   source: ReactNode;
 }
