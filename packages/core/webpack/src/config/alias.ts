6aef697f89c0d576d0f9232fc57b4b1bb17c155f
Júnior Conquista <conquista.engcomp@gmail.com>
2022-12-15 12:45:55 -0300

    fix: fixed alias webpack

--- packages/core/webpack/src/config/alias.ts
+++ packages/core/webpack/src/config/alias.ts
@@ -9,8 +9,22 @@ export const aliasItems = {
   "@nimbus-ds/tokens": join(rootDir, "./packages/core/tokens"),
   "@nimbus-ds/styles": join(rootDir, "./packages/core/styles/dist"),
   "@nimbus-ds/webpack": join(rootDir, "./packages/core/webpack/src"),
+  "@nimbus-ds/checkbox": join(
+    rootDir,
+    "./packages/react/src/atomic/Checkbox/src"
+  ),
+  "@nimbus-ds/file-uploader": join(
+    rootDir,
+    "./packages/react/src/atomic/FileUploader/src"
+  ),
   "@nimbus-ds/icon": join(rootDir, "./packages/react/src/atomic/Icon/src"),
+  "@nimbus-ds/icon-button": join(
+    rootDir,
+    "./packages/react/src/atomic/IconButton/src"
+  ),
+  "@nimbus-ds/label": join(rootDir, "./packages/react/src/atomic/Label/src"),
   "@nimbus-ds/link": join(rootDir, "./packages/react/src/atomic/Link/src"),
+  "@nimbus-ds/radio": join(rootDir, "./packages/react/src/atomic/Radio/src"),
   "@nimbus-ds/skeleton": join(
     rootDir,
     "./packages/react/src/atomic/Skeleton/src"
@@ -19,6 +33,21 @@ export const aliasItems = {
     rootDir,
     "./packages/react/src/atomic/Spinner/src"
   ),
+  "@nimbus-ds/tag": join(rootDir, "./packages/react/src/atomic/Tag/src"),
   "@nimbus-ds/text": join(rootDir, "./packages/react/src/atomic/Text/src"),
+  "@nimbus-ds/textarea": join(
+    rootDir,
+    "./packages/react/src/atomic/Textarea/src"
+  ),
+  "@nimbus-ds/thumbnail": join(
+    rootDir,
+    "./packages/react/src/atomic/Thumbnail/src"
+  ),
+  "@nimbus-ds/tooltip": join(
+    rootDir,
+    "./packages/react/src/atomic/Tooltip/src"
+  ),
   "@nimbus-ds/toggle": join(rootDir, "./packages/react/src/atomic/Toggle/src"),
+  "@nimbus-ds/alert": join(rootDir, "./packages/react/src/composite/Alert/src"),
+  "@nimbus-ds/card": join(rootDir, "./packages/react/src/composite/Card/src"),
 };
