965a261e6b47901a3a93b7a8f5dde7bf97a11f79
Júnior Conquista <conquista.engcomp@gmail.com>
2023-03-13 11:43:15 -0300

    chore: created file for updating releases

--- packages/core/styles/webpack.config.ts
+++ packages/core/styles/webpack.config.ts
@@ -1,13 +1,11 @@
 import path from "path";
 
-// import { plugins, rules, configuration, utils } from "@nimbus-ds/webpack/src";
-import { rules, configuration, plugins } from "@nimbus-ds/webpack/src";
+import { plugins, rules, configuration, utils } from "@nimbus-ds/webpack/src";
 
 const baseConfig = {
-  // entry: {
-  //   "./themes/dark": "./src/themes/nimbus-theme-dark.css.ts",
-  // },
-  // mode: "development",
+  entry: {
+    "./themes/dark": "./src/themes/nimbus-theme-dark.css.ts",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     library: "@nimbus-ds/styles",
@@ -21,12 +19,12 @@ config.plugins = [
   plugins.vanillaExtractPlugin,
   plugins.miniCssExtractPlugin,
   plugins.cssHashRemoverPlugin,
-  // plugins.dtsBundleGeneratorPlugin({
-  //   entries: [
-  //     `node ${utils.rootDir}/node_modules/.bin/dts-bundle-generator -o ./dist/index.d.ts ./src/index.ts`,
-  //     `node ${utils.rootDir}/node_modules/.bin/dts-bundle-generator -o ./dist/themes/dark.d.ts ./src/themes/dark.css.ts`,
-  //   ],
-  // }),
+  plugins.dtsBundleGeneratorPlugin({
+    entries: [
+      `node ${utils.rootDir}/node_modules/.bin/dts-bundle-generator -o ./dist/index.d.ts ./src/index.ts`,
+      `node ${utils.rootDir}/node_modules/.bin/dts-bundle-generator -o ./dist/themes/dark.d.ts ./src/themes/nimbus-theme-dark.css.ts`,
+    ],
+  }),
 ];
 
 export default () => config;
