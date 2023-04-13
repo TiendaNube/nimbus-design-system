79b6ce1722c24ccbbd5b8298d31dc427833a5d46
Júnior Conquista <conquista.engcomp@gmail.com>
2023-04-13 16:14:02 -0300

    feat: added width and height properties to the IconButton.Skeleton subcomponent api

--- packages/react/src/atomic/IconButton/src/components/IconButtonSkeleton/IconButtonSkeleton.tsx
+++ packages/react/src/atomic/IconButton/src/components/IconButtonSkeleton/IconButtonSkeleton.tsx
@@ -4,11 +4,13 @@ import { Skeleton } from "@nimbus-ds/skeleton";
 import { IconButtonSkeletonProps } from "./iconButtonSkeleton.types";
 
 const IconButtonSkeleton: React.FC<IconButtonSkeletonProps> = ({
+  width,
+  height,
   "data-testid": dataTestId,
 }) => (
   <Skeleton
-    width="2.75rem"
-    height="2.75rem"
+    width={width ?? "2.75rem"}
+    height={height ?? "2.75rem"}
     borderRadius="100%"
     data-testid={dataTestId}
   />
--- packages/react/src/atomic/IconButton/src/components/IconButtonSkeleton/iconButtonSkeleton.types.ts
+++ packages/react/src/atomic/IconButton/src/components/IconButtonSkeleton/iconButtonSkeleton.types.ts
@@ -1,8 +1,12 @@
-export interface IconButtonSkeletonProperties {
+import { SkeletonProps } from "@nimbus-ds/skeleton";
+
+export type IconButtonSkeletonProperties = Partial<
+  Pick<SkeletonProps, "width" | "height">
+> & {
   /**
    * This is an attribute used to identify a DOM node for testing purposes.
    */
   "data-testid"?: string;
-}
+};
 
 export type IconButtonSkeletonProps = IconButtonSkeletonProperties;
