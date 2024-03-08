c1069d114e1a09f70f5355c31ffff85360216517
Victor Lopes <87648920+vrlopess@users.noreply.github.com>
2024-03-08 16:12:19 -0300

    chore: test (#230)
    * chore: test
    
    * chore: test
    
    * chore: test
    
    * chore: test
    
    * chore: test
    
    * fix: test

--- jest.config.ts
+++ jest.config.ts
@@ -47,6 +47,8 @@ const config: Config.InitialOptions = {
     "@nimbus-ds/sidebar": "<rootDir>/packages/react/src/composite/Sidebar/src",
     "@nimbus-ds/tabs": "<rootDir>/packages/react/src/composite/Tabs/src",
     "@nimbus-ds/table": "<rootDir>/packages/react/src/composite/Table/src",
+    "@nimbus-ds/collapsible":
+      "<rootDir>/packages/react/src/composite/Collapsible/src",
     // commons
     "^.+\\.svg$": "jest-svg-transformer",
   },
--- packages/core/styles/src/packages/composite/collapsible/nimbus-collapsible.css.ts
+++ packages/core/styles/src/packages/composite/collapsible/nimbus-collapsible.css.ts
@@ -8,41 +8,38 @@ import { varsThemeBase } from "../../../themes";
 const body = style({
   width: "100%",
   display: "flex",
-  backgroundColor: varsThemeBase.colors.neutral.background,
   boxSizing: "border-box",
-  fontFamily: varsThemeBase.fontFamily.sans,
+  fontFamily: varsThemeBase.fontFamily.sans
 });
 
 const open = keyframes({
   from: { transform: "translateY(-10%)", opacity: 0 },
-  to: { transform: "translateY(0%)", opacity: 1 },
+  to: { transform: "translateY(0%)", opacity: 1 }
 });
 
 const bodyAnimation = style({
   width: "100%",
   display: "flex",
-  backgroundColor: varsThemeBase.colors.neutral.background,
   boxSizing: "border-box",
   fontFamily: varsThemeBase.fontFamily.sans,
-  animation: `${open} .4s`,
+  animation: `${open} .3s`
 });
 
 const openReversal = keyframes({
   from: { transform: "translateY(10%)", opacity: 0 },
-  to: { transform: "translateY(0%)", opacity: 1 },
+  to: { transform: "translateY(0%)", opacity: 1 }
 });
 
 const bodyReversalAnimation = style({
   width: "100%",
   display: "flex",
-  backgroundColor: varsThemeBase.colors.neutral.background,
   boxSizing: "border-box",
   fontFamily: varsThemeBase.fontFamily.sans,
-  animation: `${openReversal} .3s`,
+  animation: `${openReversal} .3s`
 });
 
 export const styles = {
   body,
   bodyAnimation,
-  bodyReversalAnimation,
+  bodyReversalAnimation
 };
