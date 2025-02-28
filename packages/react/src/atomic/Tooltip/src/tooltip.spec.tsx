f2fdd534bd7343a9716b322cb006adee560e72d2
Joaquin Tornello <jtornello2019@hotmail.com>
2025-02-28 09:49:23 -0300

    feat: added sprinkle test

--- packages/react/src/atomic/Tooltip/src/tooltip.spec.tsx
+++ packages/react/src/atomic/Tooltip/src/tooltip.spec.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { render, screen, fireEvent, act } from "@testing-library/react";
+import { tooltip as tooltipStyles } from "@nimbus-ds/styles";
 
 import { Tooltip } from "./Tooltip";
 import { TooltipProps } from "./tooltip.types";
 
 const makeSut = (rest: Omit<TooltipProps, "children">) => {
   render(
-    <Tooltip {...rest} data-testid="tooltip-element" maxWidth="250px">
+    <Tooltip {...rest} data-testid="tooltip-element">
       <p data-testid="anchor-element">hover</p>
     </Tooltip>
   );
@@ -96,15 +97,45 @@ describe("GIVEN <Tooltip />", () => {
       expect(arrow.style.position).toEqual("absolute");
     });
 
-    it('THEN should not display arrow if "arrow" is not passed', async () => {
+    it('should not display arrow if "arrow" is not passed', async () => {
       makeSut({ content: "string" });
+
       await act(() => {
         fireEvent.mouseEnter(screen.getByTestId("tooltip-container"));
       });
+
       const tooltip = screen.getByTestId("tooltip-element");
       const arrow = screen.queryByTestId("arrow-element");
+
       expect(tooltip).toBeDefined();
       expect(arrow).toBeNull();
+    });
+
+    it("should call tooltip.sprinkle with the merged props", () => {
+      const maxWidth = "400px";
+      const rest = { customProp: "value" };
+
+      // Set up a spy on tooltip.sprinkle.
+      const sprinkleSpy = jest
+        .spyOn(tooltipStyles, "sprinkle")
+        .mockReturnValue({
+          className: "mock-class",
+          style: { maxWidth },
+          otherProps: { "data-test": "value" },
+        });
+
+      // Render the component with both maxWidth and the rest props.
+      makeSut({ content: "string", maxWidth, ...rest });
+
+      //  Verify that tooltip.sprinkle was called with the expected merged object.
+      expect(sprinkleSpy).toHaveBeenCalledWith({
+        ...rest,
+        maxWidth,
+        "data-testid": "tooltip-element",
+      });
+
+      // Cleanup
+      sprinkleSpy.mockRestore();
     });
   });
 });
