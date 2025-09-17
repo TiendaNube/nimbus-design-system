a7f9963f7e45ddb6a56933e7e3ea1fc456ee0218
Joaquin Tornello <jtornello2019@hotmail.com>
2025-09-17 09:34:41 -0300

    fix: removed unnecessary mapping

--- packages/core/styles/src/properties/base.ts
+++ packages/core/styles/src/properties/base.ts
@@ -43,5 +43,4 @@ export const baseColors = {
   "neutral-textDisabled": varsThemeBase.colors.neutral.textDisabled,
   "neutral-textLow": varsThemeBase.colors.neutral.textLow,
   "neutral-textHigh": varsThemeBase.colors.neutral.textHigh,
-  "ai-generativeInteractive": varsThemeBase.colors.aiGenerative.interactive
 };
