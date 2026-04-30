692f945c02f3f8892dd5b1872d50535dd0b59fa2
Joaquin Tornello <jtornello2019@hotmail.com>
2026-04-30 09:53:45 -0300

    test: adds provenance

--- packages/core/scripts/src/packagePublisher/core/npm-utils/npm-utils.ts
+++ packages/core/scripts/src/packagePublisher/core/npm-utils/npm-utils.ts
@@ -121,7 +121,7 @@ function publishToNpm(
     const tagFlag = `--tag ${tag}`;
     const otpFlag = otp ? ` --otp=${otp}` : "";
 
-    const command = `npm publish --workspace=${packageName} ${accessFlag} ${tagFlag}${otpFlag}`;
+    const command = `npm publish --workspace=${packageName} ${accessFlag} ${tagFlag} --provenance${otpFlag}`;
 
     console.log(`📤 Publishing with command: ${command}`);
 
